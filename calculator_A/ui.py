8dbd73e5bdcd986a7c073938ec182280d528a6af
kkssbbb <03337@naver.com>
2024-08-02 17:00:19 +0900

    Add lbl1 in ui.py to print version info

--- calculator_A/ui.py
+++ calculator_A/ui.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import (QApplication,QWidget,QPushButton,QVBoxLayout,
                              QMessageBox, QPlainTextEdit, QHBoxLayout,
-                             QLineEdit, QComboBox)
+                             QLineEdit, QComboBox,QLabel)
 from PyQt5.QtGui import QIcon
 from PyQt5 import QtCore
 
@@ -14,7 +14,8 @@ class View(QWidget):
     def initUI(self):
         self.te1 = QPlainTextEdit()
         self.te1.setReadOnly(True)
-        
+                
+        self.lbl1 = QLabel("v2.3.0",self)
         self.btn1=QPushButton('Calc',self)
         self.btn2=QPushButton('Clear',self)
         
@@ -37,6 +38,7 @@ class View(QWidget):
         
         hbox = QHBoxLayout()
         hbox.addStretch(1)
+        hbox.addWidget(self.lbl1)
         hbox.addWidget(self.btn1)
         hbox.addWidget(self.btn2)
         
